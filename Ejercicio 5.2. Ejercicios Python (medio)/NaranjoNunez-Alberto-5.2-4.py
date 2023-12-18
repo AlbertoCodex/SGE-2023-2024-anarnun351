da5487a7ad14fe703656c983617c25a2d9ff840c
Alberto Naranjo <zenycs3@gmail.com>
2023-12-18 18:02:50 +0100

    Factorial de un numero

---
+++ Ejercicio 5.2. Ejercicios Python (medio)/NaranjoNunez-Alberto-5.2-4.py
@@ -0,0 +1,10 @@
+import math
+# 4A Escribir una función que reciba un número entero positivo y devuelva su factorial.
+def factorialNum(num):
+    print("El factorial de 7 es:", math.factorial(num))
+
+def main():
+    factorialNum(7)
+
+if __name__ == '__main__':
+    main()
