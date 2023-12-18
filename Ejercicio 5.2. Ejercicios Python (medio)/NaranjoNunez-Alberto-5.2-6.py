948c757eb2f5d466f0165b442c53bb996745376b
Alberto Naranjo <zenycs3@gmail.com>
2023-12-18 19:46:43 +0100

    Crear fichero + print titulos

---
+++ Ejercicio 5.2. Ejercicios Python (medio)/NaranjoNunez-Alberto-5.2-6.py
@@ -0,0 +1,15 @@
+# 6A En Python 3 los tipos simples se pasan por valor y los compuestos por referencia.
+# Crea un ejemplo con 3 funciones que:
+#    Reciba 2 números y devuelva la suma.
+#    Reciba una lista y modifique esa misma lista (referencia) doblando los valores de todos los elementos. No tiene que devolver nada.
+#    Reciba una lista y devuelva una copia de la misma lista (referencia) doblando los valores de todos los elementos. La lista original no debe modificarse.
+
+def sumNum():
+    SystemExit(0)
+
+def main():
+    print("6. En Python 3 los tipos simples se pasan por valor y los compuestos por referencia.\nCrea un ejemplo con 3 funciones que:")
+    sumNum()
+
+if __name__ == '__main__':
+    main()
