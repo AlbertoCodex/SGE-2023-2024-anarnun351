9ba52341bcfa6bee2fd83d49f94e9d14791e8758
Alberto Naranjo <zenycs3@gmail.com>
2024-01-01 13:06:11 +0100

    Ejercicio 6 y 7

--- Ejercicio 5.2. Ejercicios Python (medio)/NaranjoNunez-Alberto-5.2-6.py
+++ Ejercicio 5.2. Ejercicios Python (medio)/NaranjoNunez-Alberto-5.2-6.py
@@ -4,12 +4,33 @@
 #    Reciba una lista y modifique esa misma lista (referencia) doblando los valores de todos los elementos. No tiene que devolver nada.
 #    Reciba una lista y devuelva una copia de la misma lista (referencia) doblando los valores de todos los elementos. La lista original no debe modificarse.
 
-def sumNum():
-    SystemExit(0)
+def sumNum(num1, num2):
+    print("Reciba 2 números y devuelva la suma.")
+    print("La suma de", num1 ,"y", num2, "=", num1+num2)
+    print("---------------------------------------------------------------------")
+
+def modList(lista):
+    print("Reciba una lista y modifique esa misma lista (referencia) doblando los valores de todos los elementos. No tiene que devolver nada.")
+    print("Valores iniciales:", lista)
+    for i in range(len(lista)):
+        lista[i] *= 2
+
+def copyList(lista):
+    print("Reciba una lista y devuelva una copia de la misma lista (referencia) doblando los valores de todos los elementos. La lista original no debe modificarse.")
+    print("Valores iniciales:", lista)
+    for i in range(len(lista)):
+        lista[i] *= 2
 
 def main():
+    lista1 = [1,4,6,8,3]
+    nl = [1,4,6,8,3]
     print("6. En Python 3 los tipos simples se pasan por valor y los compuestos por referencia.\nCrea un ejemplo con 3 funciones que:")
-    sumNum()
+    sumNum(4,9)
+    modList(lista1)
+    print("Valores modificados", lista1)
+    print("---------------------------------------------------------------------")
+    copyList(nl[:])
+    print("Los valores no se han modificado", nl)
 
 if __name__ == '__main__':
     main()
---
+++ Ejercicio 5.2. Ejercicios Python (medio)/NaranjoNunez-Alberto-5.2-7.py
@@ -0,0 +1,15 @@
+# 7 Partiendo de un contexto donde queremos almacenar un usuario y su contraseña.  Haz un ejemplo que explique cómo se haría:
+#    Utilizando una lista.
+#    Utilizando un diccionario.
+
+def userList():
+    user = []
+
+def userDict():
+
+def main():
+    userList()
+    userDict()
+
+if __name__ == '__main__':
+    main()
