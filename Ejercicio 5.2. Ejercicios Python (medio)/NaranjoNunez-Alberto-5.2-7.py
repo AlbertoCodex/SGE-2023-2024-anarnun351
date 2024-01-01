fa4d60b3e33311c6755d22432b95ee889de6e5ba
Alberto Naranjo <zenycs3@gmail.com>
2024-01-01 17:55:59 +0100

    password with hashlib

--- Ejercicio 5.2. Ejercicios Python (medio)/NaranjoNunez-Alberto-5.2-7.py
+++ Ejercicio 5.2. Ejercicios Python (medio)/NaranjoNunez-Alberto-5.2-7.py
@@ -1,13 +1,30 @@
 # 7 Partiendo de un contexto donde queremos almacenar un usuario y su contraseña.  Haz un ejemplo que explique cómo se haría:
 #    Utilizando una lista.
 #    Utilizando un diccionario.
+import hashlib
+import random
 
 def userList():
+    print("Utilizando una lista.")
     user = []
+    for x in range(5):
+        password = hashlib.sha1(bytes(random.randint(100000, 99999999)))
+        user.append([[f"user{x}", password]])
+    print(user[1])
+    print(user[3])
+    print("---------------------------------------------------------------------")
 
 def userDict():
+    print("Utilizando un diccionario.")
+    user = {}
+    for x in range(5):
+        password = hashlib.sha1(bytes(random.randint(100000, 99999999)))
+        user[f"user{x}"] = password
+    print("user1", user["user1"])
+    print("user3", user["user3"])
 
 def main():
+    print("7 Partiendo de un contexto donde queremos almacenar un usuario y su contraseña.  Haz un ejemplo que explique cómo se haría:")
     userList()
     userDict()
 
